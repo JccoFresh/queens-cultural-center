cede97b4f2cfa67ce64d3c55f2a1c2c18af7bda6
Caleb Adorno <caleba5357@afsenyc.org>
2018-11-17 20:57:28 +0000

    all sections updated

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("contain");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
